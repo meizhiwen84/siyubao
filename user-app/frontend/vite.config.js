59cf0c72506e58560b6a47ca50b29bf02553e1cd
meizhiwen84@163.com <meizhiwen84@163.com>
2026-04-18 18:17:28 +0800

    修改

--- user-app/frontend/vite.config.js
+++ user-app/frontend/vite.config.js
@@ -7,6 +7,30 @@ export default defineConfig({
   base: '/app/',
   build: {
     outDir: path.resolve(__dirname, '../src/main/resources/static/app'),
-    emptyOutDir: true
+    emptyOutDir: true,
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true
+      },
+      mangle: {
+        toplevel: true,
+        properties: {
+          regex: /^_/
+        }
+      }
+    },
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'vue-vendor': ['vue', 'vue-router'],
+          'other-vendor': ['html-to-image']
+        },
+        entryFileNames: 'assets/[name]-[hash].js',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        assetFileNames: 'assets/[name]-[hash].[ext]'
+      }
+    }
   }
 })
